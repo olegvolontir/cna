4941834096f45e3460fb15db1fdba64727a6b086
olegvolontir <oleg.volontir20@gmail.com>
2021-03-23 09:25:09 +0200

    Added User project + remade some things

---
+++ UserData/User/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new UserData.UserDataClient(channel);
+
+            Console.WriteLine("Introduceti numele:");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti cnp:");
+            string cnp = Console.ReadLine();
+
+            var reply = await client.GetUserDataAsync(new SendUserData { Cnp = cnp, Name = nume });
+
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
