5b748f5de79946b760fcccedc3cd9faf8d2b9053
olegvolontir <73349437+olegvolontir@users.noreply.github.com>
2021-04-20 15:29:51 +0300

    Final commit

--- Tema2/ZodiacClient/ZodiacClient/Program.cs
+++ Tema2/ZodiacClient/ZodiacClient/Program.cs
@@ -13,14 +13,25 @@ namespace ZodiacClient
 
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new ZodiacSigns.ZodiacSignsClient(channel);
+            bool finished = false;
 
-            Console.WriteLine("Write a date (mm/dd/yyyy or (m/d/yyyy):");
-            string input = Console.ReadLine();
-
-            if (dateValidation.IsDateValid(input))
+            while (!finished)
             {
-                ZodiacSign sign = await client.GetZodiacSignAsync(new ZodiacSignRequest { Date = input });
-                Console.WriteLine("The zodiac sign corespoding to the date: " + sign.Sign);
+                Console.WriteLine("Write a date (mm/dd/yyyy or (m/d/yyyy):");
+                string input = Console.ReadLine();
+
+                if (dateValidation.IsDateValid(input))
+                {
+                    ZodiacSign sign = await client.GetZodiacSignAsync(new ZodiacSignRequest { Date = input });
+                    Console.WriteLine("The zodiac sign corespoding to the date: " + sign.Sign);
+                }
+
+                Console.WriteLine("1.Continue\n2.Exit");
+                int choice = int.Parse(Console.ReadLine());
+                if(choice==2)
+                {
+                    finished = true;
+                }
             }
         }
     }
